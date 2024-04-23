034ee4b224f20b17b42df26978609897021fa274
Pablo Sazo <pablo@glooadvertising.com.au>
2024-04-23 12:02:16 +1000

    url

--- front-page.php
+++ front-page.php
@@ -2,7 +2,7 @@
 <?php get_template_part( 'template-parts/catalogue' );?>
 <div class="container mx-auto my-8">
 	<img src="<?php echo get_template_directory_uri( ); ?>/img/header.png" alt="">
-<a href="<?php get_site_url(); ?>/careers"><img src="<?php echo get_template_directory_uri( ); ?>/img/join-our-team-long-banner.png" alt="" class="my-6 mx-auto"></a>
+<a href="<?php get_site_url('/'); ?>/careers"><img src="<?php echo get_template_directory_uri( ); ?>/img/join-our-team-long-banner.png" alt="" class="my-6 mx-auto"></a>
 	<?php if ( have_posts() ) : ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 			<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
