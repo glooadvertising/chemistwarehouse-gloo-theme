1f895f98da3eacc40c3976ae60f2827d4d3613db
Pablo Sazo <pablo@glooadvertising.com.au>
2024-04-29 16:18:03 +1000

    job

--- template-parts/job.php
+++ template-parts/job.php
@@ -3,6 +3,8 @@
         <summary class="bg-cwblue text-white font-bold uppercase p-4 cursor-pointer"><?php the_title(); ?></summary>
         <div class="py-4 px-6 bg-gray-100">
             <?php the_content(); ?>
+            <hr>
+            <?php echo do_shortcode( '[fluentform id="4"]' ); ?>
         </div>
     </details>
 </article>
