b6f7b1354225d45da80768d0d62a4610cc39c751
Pablo Sazo <pablo@glooadvertising.com.au>
2024-05-01 10:37:59 +1000

    job fix

--- template-parts/job.php
+++ template-parts/job.php
@@ -1,5 +1,5 @@
-<article id="post-<?php the_ID(); ?>" class="job flex flex-col gap-6 border-b-2 border-white">
-    <details class="flex flex-col justify-start items-start w-full gap-4">
+<article id="post-<?php the_ID(); ?>" class="flex flex-col gap-6 border-b-2 border-white">
+    <details class="job flex flex-col justify-start items-start w-full gap-4">
         <summary class="bg-cwblue text-white font-bold uppercase p-4 cursor-pointer"><?php the_title(); ?></summary>
         <div class="py-4 px-6 bg-gray-100">
             <?php the_content(); ?>
