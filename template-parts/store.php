f3597b5d1eaf86d13f4b6c7001faa40a7f5c33ef
Pablo Sazo <pablo@glooadvertising.com.au>
2024-05-01 10:19:03 +1000

    more changes

--- template-parts/store.php
+++ template-parts/store.php
@@ -2,12 +2,12 @@
     <div class="col-span-5 sm:col-span-3 flex flex-col justify-start items-start w-full gap-4">
         <h2 class="text-cwblue font-bold uppercase"><?php the_title(); ?></h2>
         <?php the_content(); ?>
-        <?php $custom = get_post_custom();?>
+        <!--?php $custom = get_post_custom();?>
         <p>
             <?php if(isset($custom['phone'])) { echo '<strong>PH:</strong> <a href="tel:'.$custom['phone'][0].'">'.$custom['phone'][0].'</a><br>';}?>
             <?php if(isset($custom['healthmail'])) { echo '<strong>Healthmail:</strong> <a href="mailto:'.$custom['healthmail'][0].'">'.$custom['healthmail'][0].'</a><br>';}?>
         </p>
-        <div class="flex flex-col">
+        <div-- class="flex flex-col">
             <h3 class="text-cwblue font-bold">Trading hours</h3>
             <p>
                 <?php if(isset($custom['mon_fri'])) { echo '<strong>Monday - Friday:</strong> '.$custom['mon_fri'][0]. '<br>';}?>
@@ -15,7 +15,7 @@
                 <?php if(isset($custom['sat'])) { echo '<strong>Saturday:</strong> '.$custom['sat'][0];}?><br>
                 <?php if(isset($custom['sun_ph'])) { echo '<strong>Sunday & Public Holidays:</strong> '.$custom['sun_ph'][0]. '<br>';}?>
             </p>
-        </div>
+        </div-->
     </div>
-    <img src="<?php echo the_post_thumbnail_url();?>" alt="" class="w-full col-span-5 sm:col-span-2">
+    <img src="<?php echo the_post_thumbnail_url();?>" alt="" class="w-full col-span-5 sm:col-span-2 border-2">
 </article>
